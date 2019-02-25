11abe5ee0f9fd65e80ce0b10352e8c39e75bdd16
David Murphy <dmurphy4@molloy.edu>
2019-02-25 17:01:09 -0500

    Add descriptions and assign new heights for the experience paper list items

--- src/Components/Layouts/Experience/Experience.js
+++ src/Components/Layouts/Experience/Experience.js
@@ -12,7 +12,7 @@ const jssStyle = theme => ({
   
   experiencePaperLeft: {
     width: '96%',
-    height: '180px',
+    height: '300px', // '300px'
     position: 'relative',
     padding: '30px',
     boxSizing: 'border-box',
@@ -30,7 +30,7 @@ const jssStyle = theme => ({
   },
   experiencePaperRight: {
     width: '96%',
-    height: '180px',
+    height: '300px', // '300px'
     position: 'relative',
     padding: '30px',
     boxSizing: 'border-box',
@@ -58,37 +58,44 @@ const Experience = (props) => {
     'Applications Developer/Implementation Specialist': {
       company: 'Molloy College',
       start: 'Jun 2018',
-      end: 'Present'
+      end: 'Present',
+      description: 'Responsible for the development, analysis, implementation, documentation, training, and ongoing support of software applications designed to improve administrative processes and overall organizational effectiveness. Develop and recommend solutions to enhance overall process improvement initiatives in both administrative and academic areas of the College.'
     },
     'Robotics Co-Advisor': {
       company: 'Hewlett-Woodmere School District',
       start: 'Sep 2017',
-      end: 'Present'
+      end: 'Present',
+      description: 'Board-certified to instruct Android mobile development for students between grades 9-12. Provide guidance for both software (Java - Android Studio & Eclipse) and hardware design and implementation in an academic setting.'
     },
     'Chief Technology Officer': {
       company: 'Skinno Inc.',
       start: 'Dec 2017',
-      end: 'Jun 2018'
+      end: 'Jun 2018',
+      description: `Develop all aspects of Skinno's multi-platform service, including a website and a mobile application for iOS devices. Monitor technology trends in machine learning and UI/UX to improve the quality of our applications. Communicate the company's technology strategy to partners, management, investors and employees.`
     },
     'Technology Specialist': {
       company: 'Molloy College',
       start: 'Nov 2017',
-      end: 'Jun 2018'
+      end: 'Jun 2018',
+      description: 'Monitor, manually re-image, and connect several computer labs onto both the student and faculty DNS, assigning user and admin privileges respectively. Working Experience with switch panels and patch panels in a professional environment.'
     },
     'Undergraduate Computer Science Tutor': {
       company: 'Molloy College',
       start: 'Oct 2017',
-      end: 'Apr 2018'
+      end: 'Apr 2018',
+      description: 'Provide guidance and instruction to college students for material related to their Computer Science studies, including (but not limited to): Web Scripting, Dynamic Programming, Object Oriented Concepts, MVC Architecture, etc.'
     },
     'Undergraduate Teacher Assistant - Web Scripting': {
       company: 'Molloy College',
       start: 'Oct 2017',
-      end: 'Dec 2017'
+      end: 'Dec 2017',
+      description: 'Volunteer 5 hours per week to help instruct a college-level Web Scripting course.'
     },
     'Intern Editor': {
       company: 'Messapps',
       start: 'May 2016',
-      end: 'Feb 2017'
+      end: 'Feb 2017',
+      description: 'Collaborate with the CEO and several project managers to produce online content on a weekly basis. Written content pertained to topics in both mobile devices and the development of mobile applications. Attended meetings to contribute topic suggestions for future web content.'
     }
   }
 
@@ -113,7 +120,10 @@ const Experience = (props) => {
       }
       return(<li className={float + ' ' + styles.experienceItem} style={largeMarginTop} key={jobName}>
         <Paper className={float + ' ' + experiencePaperOrientation}>
-          <Typography variant={'body1'}>{jobName}</Typography>
+          <Typography variant={'body1'}>{experienceJSON[jobName].company}</Typography>          
+          <Typography variant={'h5'}>{jobName}</Typography>
+          <Typography variant={'body1'}>{experienceJSON[jobName].start + ' - ' + experienceJSON[jobName].end}</Typography>
+          <Typography variant={'body2'}>{experienceJSON[jobName].description}</Typography>
         </Paper>
       </li>)
     });
