a0f113857e1f1a7f36c5aac6a91188bc59912d68
David Murphy <dmurphy4@molloy.edu>
2019-02-25 15:55:37 -0500

    refactor education.js and experience.js to make paper list item generation more readable

--- src/Components/Layouts/Education/Education.js
+++ src/Components/Layouts/Education/Education.js
@@ -1,13 +1,57 @@
-import React, {Fragment} from 'react'
+import React, {Fragment} from 'react';
+import styles from './Education.module.css' // css modules
+import { withStyles } from '@material-ui/styles'; // jss library
 
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import Timeline from '../../Timeline/Timeline'
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Timeline from '../../Timeline/Timeline';
 import Paper from '@material-ui/core/Paper';
 
-import styles from './Education.module.css'
+const jssStyle = theme => ({
+  educationPaperLeft: {
+    width: '96%',
+    height: '180px',
+    position: 'relative',
+    padding: '30px',
+    boxSizing: 'border-box',
+    '&:before': {
+      position: 'absolute',
+      top: '26px',
+      right: '-15px',
+      display: 'inline-block',
+      borderTop: '15px solid transparent',
+      borderLeft: '15px solid #ffffff',
+      borderRight: '0 solid #ffffff',
+      borderBottom: '15px solid transparent',
+      content: "''"
+    }
+  },
+  educationPaperRight: {
+    width: '96%',
+    height: '180px',
+    position: 'relative',
+    padding: '30px',
+    boxSizing: 'border-box',
+    '&:before': {
+      position: 'absolute',
+      top: '26px',
+      left: '-15px',
+      right: 'auto',
+      display: 'inline-block',
+      borderTop: '15px solid transparent',
+      borderLeft: '15px solid #ffffff',
+      borderRight: '0 solid #ffffff',
+      borderBottom: '15px solid transparent',
+      content: "''",
+      borderLeftWidth: '0',
+      borderRightWidth: '15px'
+    }
+  }
+})
 
-const Education = () => {
+const Education = (props) => {
+
+  const {classes} = props;
 
   const educationJSON = {
     "Molloy College": {
@@ -28,41 +72,35 @@ const Education = () => {
   }
 
   const generateEducationListItems = () => {
+    
     let i=0;
     // i%2=0 means even, which means a right-hand list item. So styleRef[0] will contain right-hand <li> jss
     return Object.keys(educationJSON).map(function(collegeName) {
       i++;
-      
-      let largeMarginTop = null;
 
-      if(i===2) {
+      // Default style = list item oriented to the right
+      let largeMarginTop = null;
+      let float = styles.floatRight;
+      let educationPaperOrientation = classes.educationPaperRight;
+
+      if(i%2===1) { // left list item
+        float = styles.floatLeft;
+        educationPaperOrientation = classes.educationPaperLeft;
+      } else if(i===2) {
         largeMarginTop = {
           marginTop: '50px'
         }
       }
 
-      // degree, major, start, end, gpa
-      if(i%2===1) { // left list item
-        return(
-        <li className={styles.floatLeft + ' ' + styles.educationItem} key={collegeName}>
-          <Paper className={styles.floatLeft + ' ' + styles.educationPaperLeft}>
-            <Typography>{collegeName}</Typography>
-            <Typography variant={'h5'}>{educationJSON[collegeName].degree +' in ' +educationJSON[collegeName].major}</Typography>
-            <Typography variant={'body1'}>{educationJSON[collegeName].start + ' - ' + educationJSON[collegeName].end}</Typography>
-            <Typography variant={'body1'}>{educationJSON[collegeName].gpa}</Typography>
-          </Paper>
-        </li>)
-      } else { // right list item
-        return(
-        <li className={styles.floatRight + ' ' + styles.educationItem} style={largeMarginTop} key={collegeName}>
-          <Paper className={styles.floatRight + ' ' + styles.educationPaperRight}>
-            <Typography>{collegeName}</Typography>
-            <Typography variant={'h5'}>{educationJSON[collegeName].degree +' in ' +educationJSON[collegeName].major}</Typography>
-            <Typography variant={'body1'}>{educationJSON[collegeName].start + ' - ' + educationJSON[collegeName].end}</Typography>
-            <Typography variant={'body1'}>{educationJSON[collegeName].gpa}</Typography>
-          </Paper>
-        </li>)
-      }
+      return (
+      <li className={float + ' ' + styles.educationItem} key={collegeName} style={largeMarginTop}>
+        <Paper className={float + ' ' + educationPaperOrientation}>
+          <Typography>{collegeName}</Typography>
+          <Typography variant={'h5'}>{educationJSON[collegeName].degree +' in ' + educationJSON[collegeName].major}</Typography>
+          <Typography variant={'body1'}>{educationJSON[collegeName].start + ' - ' + educationJSON[collegeName].end}</Typography>
+          <Typography variant={'body1'}>{educationJSON[collegeName].gpa}</Typography>
+        </Paper>
+      </li>)
     });
   }
 
@@ -76,4 +114,4 @@ const Education = () => {
   )
 }
 
-export default Education
+export default withStyles(jssStyle)(Education)
--- src/Components/Layouts/Experience/Experience.js
+++ src/Components/Layouts/Experience/Experience.js
@@ -1,13 +1,58 @@
 import React, {Fragment} from 'react';
+import { withStyles } from '@material-ui/styles'; // jss library
+import styles from './Experience.module.css' // css modules
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
 import Timeline from '../../Timeline/Timeline';
-import styles from './Experience.module.css'
 
-const Experience = () => {
+const jssStyle = theme => ({
+  
+  experiencePaperLeft: {
+    width: '96%',
+    height: '180px',
+    position: 'relative',
+    padding: '30px',
+    boxSizing: 'border-box',
+    '&:before': {
+      position: 'absolute',
+      top: '26px',
+      right: '-15px',
+      display: 'inline-block',
+      borderTop: '15px solid transparent',
+      borderLeft: '15px solid #ffffff',
+      borderRight: '0 solid #ffffff',
+      borderBottom: '15px solid transparent',
+      content: "''"
+    }
+  },
+  experiencePaperRight: {
+    width: '96%',
+    height: '180px',
+    position: 'relative',
+    padding: '30px',
+    boxSizing: 'border-box',
+    '&:before': {
+      position: 'absolute',
+      top: '26px',
+      left: '-15px',
+      right: 'auto',
+      display: 'inline-block',
+      borderTop: '15px solid transparent',
+      borderLeft: '15px solid #ffffff',
+      borderRight: '0 solid #ffffff',
+      borderBottom: '15px solid transparent',
+      content: "''",
+      borderLeftWidth: '0',
+      borderRightWidth: '15px'
+    }
+  }
+})
+
+const Experience = (props) => {
+  const {classes} = props;
 
   const experienceJSON = {
     'Applications Developer/Implementation Specialist': {
@@ -47,33 +92,30 @@ const Experience = () => {
     }
   }
 
-  const generateExperienceListItems = (props) => {
+  const generateExperienceListItems = () => {
     let i=0;
     // i%2=0 means even, which means a right-hand list item. So styleRef[0] will contain right-hand <li> jss
     return Object.keys(experienceJSON).map(function(jobName) {
       i++;
       
+      // Default style = list item oriented to the right
       let largeMarginTop = null;
+      let float = styles.floatRight;
+      let experiencePaperOrientation = classes.experiencePaperRight;
 
-      if(i===2) {
+      if(i%2===1) { // left list item
+        float = styles.floatLeft;
+        experiencePaperOrientation = classes.experiencePaperLeft;
+      } else if(i===2) {
         largeMarginTop = {
           marginTop: '50px'
         }
       }
-
-      if(i%2===1) { // shift list item to the left
-        return(<li className={styles.floatLeft + ' ' + styles.experienceItem} key={jobName}>
-          <Paper className={styles.floatLeft + ' ' + styles.experiencePaperLeft}>
-            <Typography variant={'body1'}>{jobName}</Typography>
-          </Paper>
-        </li>)
-      } else { // shift list item to the right
-        return(<li className={styles.floatRight + ' ' + styles.experienceItem} style={largeMarginTop} key={jobName}>
-          <Paper className={styles.floatRight + ' ' + styles.experiencePaperRight}>
-            <Typography variant={'body1'}>{jobName}</Typography>
-          </Paper>
-        </li>)
-      }
+      return(<li className={float + ' ' + styles.experienceItem} style={largeMarginTop} key={jobName}>
+        <Paper className={float + ' ' + experiencePaperOrientation}>
+          <Typography variant={'body1'}>{jobName}</Typography>
+        </Paper>
+      </li>)
     });
   }
 
@@ -87,4 +129,4 @@ const Experience = () => {
   )
 }
 
-export default Experience
+export default withStyles(jssStyle)(Experience)
