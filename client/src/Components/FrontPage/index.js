fa3e0c15a6fe3252ebb1e6e6736a69fadefdb971
David Murphy <djmgeneseo@gmail.com>
2019-06-08 16:57:58 -0400

    remove unecessary subdirectories for production usage. Update gitignore

--- client/src/Components/FrontPage/index.js
+++
@@ -1,7 +0,0 @@
-import Abilities from './Abilities'
-import AchievementCard from './AchievementCard'
-import Timeline from './Timeline'
-
-export {
-    Abilities, AchievementCard, Timeline
-}
--- client/src/Containers/Blogs/index.js
+++
@@ -1,6 +0,0 @@
-import Blogs from './Blogs'
-import MessAppsBlog from './MessAppsBlog'
-
-export {
-    Blogs, MessAppsBlog
-}
